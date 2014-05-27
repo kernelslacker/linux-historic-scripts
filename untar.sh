9d33dafc5aa27aef48179da082565f509a37f958
Dave Jones <davej@redhat.com>
2014-05-27 19:57:25 -0400

    commentary

--- untar.sh
+++ untar.sh
@@ -4,6 +4,8 @@
 if [ -f pre-0.96.tar.bz2 ]; then
 	mv pre-0.96.tar.bz2 linux-0.96pre.tar.bz2
 fi
+
+# Seeing as we're renaming and unpacking, repack them as bzips to save some space.
 for i in 0.99.1 0.99.2 0.99.3 0.99.4 0.99.5 0.99.6 0.99.7 0.99.7A 0.99.8 0.99.9 0.99.10
 do
 	if [ -f linux-$i.tar.z ]; then
