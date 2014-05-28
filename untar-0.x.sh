4c2321ca4cfee64e41ab85c6b16ce8a8c1f32cc0
Dave Jones <davej@redhat.com>
2014-05-27 20:04:19 -0400

    fix up untarring of 0.x trees

--- untar-0.x.sh
+++ untar-0.x.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+FROM=$(pwd)/binaries
+
 if [ ! -d unpack ]; then
   echo run from wrong dir.
   exit
@@ -9,10 +11,10 @@ fi
 
 untar()
 {
-        echo Unpacking $1
-        tar jxf ../0.x/linux-$1.tar.bz2
+	echo Unpacking $1
+	tar jxf $FROM/0.x/linux-$1.tar.bz2
 	if [ -d linux ]; then
-        	mv linux linux-$1
+		mv linux linux-$1
 	fi
 }
 
@@ -28,46 +30,46 @@ done
 echo patching to 0.96a-patch1
 cp -rl linux-0.96a linux-0.96a-patch1
 cd linux-0.96a-patch1
-bzcat ../../0.x/linux-0.96a.patch1.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96a.patch1.bz2 | patch -p1 -s
 cd ..
 echo patching to 0.96a-patch2
 cp -rl linux-0.96a-patch1 linux-0.96a-patch2
 cd linux-0.96a-patch2
-bzcat ../../0.x/linux-0.96a.patch2.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96a.patch2.bz2 | patch -p1 -s
 cd ..
 echo patching to 0.96a-patch3
 cp -rl linux-0.96a-patch2 linux-0.96a-patch3
 cd linux-0.96a-patch3
-bzcat ../../0.x/linux-0.96a.patch3.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96a.patch3.bz2 | patch -p1 -s
 cd ..
 echo patching to 0.96a-patch4
 cp -rl linux-0.96a-patch3 linux-0.96a-patch4
 cd linux-0.96a-patch4
-bzcat ../../0.x/linux-0.96a.patch4.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96a.patch4.bz2 | patch -p1 -s
 cd ..
 
 untar 0.96b
 echo patching to 0.96b-patch1
 cp -rl linux-0.96b linux-0.96b-patch1
 cd linux-0.96b-patch1
-bzcat ../../0.x/linux-0.96b.patch1.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96b.patch1.bz2 | patch -p1 -s
 cd ..
 echo patching to 0.96b-patch2
 cp -rl linux-0.96b-patch1 linux-0.96b-patch2
 cd linux-0.96b-patch2
-bzcat ../../0.x/linux-0.96b.patch2.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96b.patch2.bz2 | patch -p1 -s
 cd ..
 
 untar 0.96c
 echo patching to 0.96c-patch1
 cp -rl linux-0.96c linux-0.96c-patch1
 cd linux-0.96c-patch1
-bzcat ../../0.x/linux-0.96c.patch1.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96c.patch1.bz2 | patch -p1 -s
 cd ..
 echo patching to 0.96c-patch2
 cp -rl linux-0.96c-patch1 linux-0.96c-patch2
 cd linux-0.96c-patch2
-bzcat ../../0.x/linux-0.96c.patch2.bz2 | patch -p1 -s
+bzcat $FROM/0.x/linux-0.96c.patch2.bz2 | patch -p1 -s
 cd ..
 
 untar 0.96pre
@@ -81,27 +83,27 @@ find linux-0.97 -type d -exec chmod +x {} \;
 echo patching to 0.97.1
 cp -rl linux-0.97 linux-0.97.1
 cd linux-0.97.1
-zcat ../../0.x/linux-0.97.patch1.gz | patch -p1 -s
+zcat $FROM/0.x/linux-0.97.patch1.gz | patch -p1 -s
 cd ..
 echo patching to 0.97.2
 cp -rl linux-0.97.1 linux-0.97.2
 cd linux-0.97.2
-zcat ../../0.x/linux-0.97.patch2.gz | patch -p1 -s
+zcat $FROM/0.x/linux-0.97.patch2.gz | patch -p1 -s
 cd ..
 echo patching to 0.97.3
 cp -rl linux-0.97.2 linux-0.97.3
 cd linux-0.97.3
-zcat ../../0.x/linux-0.97.patch3.gz | patch -p1 -s
+zcat $FROM/0.x/linux-0.97.patch3.gz | patch -p1 -s
 cd ..
 echo patching to 0.97.4
 cp -rl linux-0.97.3 linux-0.97.4
 cd linux-0.97.4
-zcat ../../0.x/linux-0.97.patch4.gz | patch -p1 -s
+zcat $FROM/0.x/linux-0.97.patch4.gz | patch -p1 -s
 cd ..
 echo patching to 0.97.5
 cp -rl linux-0.97.4 linux-0.97.5
 cd linux-0.97.5
-zcat ../../0.x/linux-0.97.patch5.gz | patch -p1 -s
+zcat $FROM/0.x/linux-0.97.patch5.gz | patch -p1 -s
 cd ..
 
 
@@ -116,21 +118,21 @@ done
 echo patching to 0.99.11-patch1
 cp -rl linux-0.99.11 linux-0.99.11-patch1
 cd linux-0.99.11-patch1
-bzcat ../../0.x/0.99.11-patch1.diff.bz2 | patch -p1 -s
+bzcat $FROM/0.x/0.99.11-patch1.diff.bz2 | patch -p1 -s
 cd ..
 
 untar 0.99.12
 echo patching to 0.99.12-patch1
 cp -rl linux-0.99.12 linux-0.99.12-patch1
 cd linux-0.99.12-patch1
-bzcat ../../0.x/0.99.12-patch1.diff.bz2 | patch -p1 -s
+bzcat $FROM/0.x/0.99.12-patch1.diff.bz2 | patch -p1 -s
 cd ..
 
 untar 0.99.13
 echo patching to 0.99.13k
 cp -rl linux-0.99.13 linux-0.99.13k
 cd linux-0.99.13k
-bzcat ../../0.x/0.99.13k.diff.bz2 | patch -p1 -s
+bzcat $FROM/0.x/0.99.13k.diff.bz2 | patch -p1 -s
 cd ..
 
 for ver in 0.99.14 0.99.14a 0.99.14b 0.99.14c 0.99.14d 0.99.14e 0.99.14f 0.99.14g 0.99.14h 0.99.14i 0.99.14j 0.99.14k 0.99.14l \
@@ -146,11 +148,11 @@ done
 echo patching to 1.0pre1
 cp -rl linux-0.99.15j linux-1.0pre1
 cd linux-1.0pre1
-bzcat ../../0.x/1.0pre1.diff.bz2 | patch -p1 -s
+bzcat $FROM/0.x/1.0pre1.diff.bz2 | patch -p1 -s
 cd ..
 echo patching to 1.0alpha
 cp -rl linux-1.0pre1 linux-1.0alpha
 cd linux-1.0alpha
-bzcat ../../0.x/1.0alpha.diff.bz2 | patch -p1 -s
+bzcat $FROM/0.x/1.0alpha.diff.bz2 | patch -p1 -s
 cd ..
 
