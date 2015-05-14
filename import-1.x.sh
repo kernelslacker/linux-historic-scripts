27fad0cbefd71009b4043139f8643bed8f514a4c
Dave Jones <davej@codemonkey.org.uk>
2015-05-13 22:02:51 -0400

    remove for loops from 1.x import script

--- import-1.x.sh
+++ import-1.x.sh
@@ -19,16 +19,117 @@ import_1_x()
 
   # Now do 1.1
   git checkout master
-  for ver in $(seq 0 95)
-  do
-    import 1.1.$ver
-  done
+
+  import 1.1.0
+  import 1.1.1
+  import 1.1.2
+  import 1.1.3
+  import 1.1.4
+  import 1.1.5
+  import 1.1.6
+  import 1.1.7
+  import 1.1.8
+  import 1.1.9
+  import 1.1.10
+  import 1.1.11
+  import 1.1.12
+  import 1.1.13
+  import 1.1.14
+  import 1.1.15
+  import 1.1.16
+  import 1.1.17
+  import 1.1.18
+  import 1.1.19
+  import 1.1.20
+  import 1.1.21
+  import 1.1.22
+  import 1.1.23
+  import 1.1.24
+  import 1.1.25
+  import 1.1.26
+  import 1.1.27
+  import 1.1.28
+  import 1.1.29
+  import 1.1.30
+  import 1.1.31
+  import 1.1.32
+  import 1.1.33
+  import 1.1.34
+  import 1.1.35
+  import 1.1.36
+  import 1.1.37
+  import 1.1.38
+  import 1.1.39
+  import 1.1.40
+  import 1.1.41
+  import 1.1.42
+  import 1.1.43
+  import 1.1.44
+  import 1.1.45
+  import 1.1.46
+  import 1.1.47
+  import 1.1.48
+  import 1.1.49
+  import 1.1.50
+  import 1.1.51
+  import 1.1.52
+  import 1.1.53
+  import 1.1.54
+  import 1.1.55
+  import 1.1.56
+  import 1.1.57
+  import 1.1.58
+  import 1.1.59
+  import 1.1.60
+  import 1.1.61
+  import 1.1.62
+  import 1.1.63
+  import 1.1.64
+  import 1.1.65
+  import 1.1.66
+  import 1.1.67
+  import 1.1.68
+  import 1.1.69
+  import 1.1.70
+  import 1.1.71
+  import 1.1.72
+  import 1.1.73
+  import 1.1.74
+  import 1.1.75
+  import 1.1.76
+  import 1.1.77
+  import 1.1.78
+  import 1.1.79
+  import 1.1.80
+  import 1.1.81
+  import 1.1.82
+  import 1.1.83
+  import 1.1.84
+  import 1.1.85
+  import 1.1.86
+  import 1.1.87
+  import 1.1.88
+  import 1.1.89
+  import 1.1.90
+  import 1.1.91
+  import 1.1.92
+  import 1.1.93
+  import 1.1.94
+  import 1.1.95
 
   # Now do 1.2
-  for ver in $(seq 0 10)
-  do
-    import 1.2.$ver
-  done
+  import 1.2.0
+  import 1.2.1
+  import 1.2.2
+  import 1.2.3
+  import 1.2.4
+  import 1.2.5
+  import 1.2.6
+  import 1.2.7
+  import 1.2.8
+  import 1.2.9
+  import 1.2.10
+
   # 1.2 branched to 1.3 at 1.2.10
   git branch 1.2
   git checkout 1.2
@@ -38,12 +139,121 @@ import_1_x()
 
   # Now do 1.3
   git checkout master
-  for ver in $(seq 0 100)
-  do
-    import 1.3.$ver
-  done
-  for ver in $(seq 1 14)
-  do
-    import pre2.0.$ver
-  done
+
+  import 1.3.0
+  import 1.3.1
+  import 1.3.2
+  import 1.3.3
+  import 1.3.4
+  import 1.3.5
+  import 1.3.6
+  import 1.3.7
+  import 1.3.8
+  import 1.3.9
+  import 1.3.10
+  import 1.3.11
+  import 1.3.12
+  import 1.3.13
+  import 1.3.14
+  import 1.3.15
+  import 1.3.16
+  import 1.3.17
+  import 1.3.18
+  import 1.3.19
+  import 1.3.20
+  import 1.3.21
+  import 1.3.22
+  import 1.3.23
+  import 1.3.24
+  import 1.3.25
+  import 1.3.26
+  import 1.3.27
+  import 1.3.28
+  import 1.3.29
+  import 1.3.30
+  import 1.3.31
+  import 1.3.32
+  import 1.3.33
+  import 1.3.34
+  import 1.3.35
+  import 1.3.36
+  import 1.3.37
+  import 1.3.38
+  import 1.3.39
+  import 1.3.40
+  import 1.3.41
+  import 1.3.42
+  import 1.3.43
+  import 1.3.44
+  import 1.3.45
+  import 1.3.46
+  import 1.3.47
+  import 1.3.48
+  import 1.3.49
+  import 1.3.50
+  import 1.3.51
+  import 1.3.52
+  import 1.3.53
+  import 1.3.54
+  import 1.3.55
+  import 1.3.56
+  import 1.3.57
+  import 1.3.58
+  import 1.3.59
+  import 1.3.60
+  import 1.3.61
+  import 1.3.62
+  import 1.3.63
+  import 1.3.64
+  import 1.3.65
+  import 1.3.66
+  import 1.3.67
+  import 1.3.68
+  import 1.3.69
+  import 1.3.70
+  import 1.3.71
+  import 1.3.72
+  import 1.3.73
+  import 1.3.74
+  import 1.3.75
+  import 1.3.76
+  import 1.3.77
+  import 1.3.78
+  import 1.3.79
+  import 1.3.80
+  import 1.3.81
+  import 1.3.82
+  import 1.3.83
+  import 1.3.84
+  import 1.3.85
+  import 1.3.86
+  import 1.3.87
+  import 1.3.88
+  import 1.3.89
+  import 1.3.90
+  import 1.3.91
+  import 1.3.92
+  import 1.3.93
+  import 1.3.94
+  import 1.3.95
+  import 1.3.96
+  import 1.3.97
+  import 1.3.98
+  import 1.3.99
+  import 1.3.100
+
+  import pre2.0.1
+  import pre2.0.2
+  import pre2.0.3
+  import pre2.0.4
+  import pre2.0.5
+  import pre2.0.6
+  import pre2.0.7
+  import pre2.0.8
+  import pre2.0.9
+  import pre2.0.10
+  import pre2.0.11
+  import pre2.0.12
+  import pre2.0.13
+  import pre2.0.14
 }
