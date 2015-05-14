5f9fe2cc879b280ac9a7b4ad9b7619d752ef4e56
Dave Jones <davej@codemonkey.org.uk>
2015-05-13 22:19:03 -0400

    fill in some blank dates in 1.x
    these came from the archive.org historic tree.
    We might be able to get more accuracy (and fill in the blanks)
    from the ftp timestamps, but that's for later.

--- import-1.x.sh
+++ import-1.x.sh
@@ -2,13 +2,13 @@
 
 import_1_x()
 {
-  import 1.0 
-  import 1.0.1 
-  import 1.0.2 
-  import 1.0.3 
-  import 1.0.4 
-  import 1.0.5 
-  import 1.0.6 
+  import 1.0 "Sat Mar 12 12:00:00 1994 -0600"
+  import 1.0.1 "Sat Mar 12 12:00:00 1994 -0600"
+  import 1.0.2 "Sat Mar 12 12:00:00 1994 -0600"
+  import 1.0.3 "Sat Mar 12 12:00:00 1994 -0600"
+  import 1.0.4 "Sat Mar 12 12:00:00 1994 -0600"
+  import 1.0.5 "Sat Mar 12 12:00:00 1994 -0600"
+  import 1.0.6 "Sat Mar 12 12:00:00 1994 -0600"
 
   # 1.0 branched to 1.1 at 1.0.6
   git branch 1.0
@@ -20,240 +20,240 @@ import_1_x()
   # Now do 1.1
   git checkout master
 
-  import 1.1.0
-  import 1.1.1
-  import 1.1.2
-  import 1.1.3
-  import 1.1.4
-  import 1.1.5
-  import 1.1.6
-  import 1.1.7
-  import 1.1.8
-  import 1.1.9
-  import 1.1.10
-  import 1.1.11
-  import 1.1.12
-  import 1.1.13
-  import 1.1.14
-  import 1.1.15
-  import 1.1.16
-  import 1.1.17
-  import 1.1.18
-  import 1.1.19
-  import 1.1.20
-  import 1.1.21
-  import 1.1.22
-  import 1.1.23
-  import 1.1.24
-  import 1.1.25
-  import 1.1.26
-  import 1.1.27
-  import 1.1.28
-  import 1.1.29
-  import 1.1.30
-  import 1.1.31
-  import 1.1.32
-  import 1.1.33
-  import 1.1.34
-  import 1.1.35
-  import 1.1.36
-  import 1.1.37
-  import 1.1.38
-  import 1.1.39
-  import 1.1.40
-  import 1.1.41
-  import 1.1.42
-  import 1.1.43
-  import 1.1.44
-  import 1.1.45
-  import 1.1.46
-  import 1.1.47
-  import 1.1.48
-  import 1.1.49
-  import 1.1.50
-  import 1.1.51
-  import 1.1.52
-  import 1.1.53
-  import 1.1.54
-  import 1.1.55
-  import 1.1.56
-  import 1.1.57
-  import 1.1.58
-  import 1.1.59
-  import 1.1.60
-  import 1.1.61
-  import 1.1.62
-  import 1.1.63
-  import 1.1.64
-  import 1.1.65
-  import 1.1.66
-  import 1.1.67
-  import 1.1.68
-  import 1.1.69
-  import 1.1.70
-  import 1.1.71
-  import 1.1.72
-  import 1.1.73
-  import 1.1.74
-  import 1.1.75
-  import 1.1.76
-  import 1.1.77
-  import 1.1.78
-  import 1.1.79
-  import 1.1.80
-  import 1.1.81
-  import 1.1.82
-  import 1.1.83
-  import 1.1.84
-  import 1.1.85
-  import 1.1.86
-  import 1.1.87
-  import 1.1.88
-  import 1.1.89
-  import 1.1.90
-  import 1.1.91
-  import 1.1.92
-  import 1.1.93
-  import 1.1.94
-  import 1.1.95
+  import 1.1.0 "Tue Apr 5 11:00:00 1994 -0600"
+  import 1.1.1 ""
+  import 1.1.2 ""
+  import 1.1.3 ""
+  import 1.1.4 ""
+  import 1.1.5 ""
+  import 1.1.6 ""
+  import 1.1.7 ""
+  import 1.1.8 ""
+  import 1.1.9 ""
+  import 1.1.10 ""
+  import 1.1.11 ""
+  import 1.1.12 ""
+  import 1.1.13 ""
+  import 1.1.14 "Sun May 22 11:00:00 1994 -0600"
+  import 1.1.15 ""
+  import 1.1.16 ""
+  import 1.1.17 ""
+  import 1.1.18 ""
+  import 1.1.19 ""
+  import 1.1.20 ""
+  import 1.1.21 ""
+  import 1.1.22 ""
+  import 1.1.23 "Sun Jun 26 11:00:00 1994 -0600"
+  import 1.1.24 ""
+  import 1.1.25 ""
+  import 1.1.26 ""
+  import 1.1.27 ""
+  import 1.1.28 ""
+  import 1.1.29 "Wed Jul 13 11:00:00 1994 -0600"
+  import 1.1.30 ""
+  import 1.1.31 ""
+  import 1.1.32 ""
+  import 1.1.33 ""
+  import 1.1.34 ""
+  import 1.1.35 ""
+  import 1.1.36 ""
+  import 1.1.37 ""
+  import 1.1.38 ""
+  import 1.1.39 ""
+  import 1.1.40 ""
+  import 1.1.41 ""
+  import 1.1.42 ""
+  import 1.1.43 ""
+  import 1.1.44 ""
+  import 1.1.45 "Sun Aug 14 11:00:00 1994 -0600"
+  import 1.1.46 ""
+  import 1.1.47 ""
+  import 1.1.48 ""
+  import 1.1.49 ""
+  import 1.1.50 ""
+  import 1.1.51 ""
+  import 1.1.52 "Wed Oct 5 11:00:00 1994 -0600"
+  import 1.1.53 ""
+  import 1.1.54 ""
+  import 1.1.55 ""
+  import 1.1.56 ""
+  import 1.1.57 ""
+  import 1.1.58 ""
+  import 1.1.59 "Thu Oct 27 11:00:00 1994 -0600"
+  import 1.1.60 ""
+  import 1.1.61 ""
+  import 1.1.62 ""
+  import 1.1.63 "Sun Nov 13 12:00:00 1994 -0600"
+  import 1.1.64 "Mon Nov 14 12:00:00 1994 -0600"
+  import 1.1.65 ""
+  import 1.1.66 ""
+  import 1.1.67 "Sun Nov 27 12:00:00 1994 -0600"
+  import 1.1.68 ""
+  import 1.1.69 ""
+  import 1.1.70 "Thu Dec 1 12:00:00 1994 -0600"
+  import 1.1.71 "Sun Dec 4 12:00:00 1994 -0600"
+  import 1.1.72 ""
+  import 1.1.73 "Wed Dec 14 12:00:00 1994 -0600"
+  import 1.1.74 "Thu Dec 22 12:00:00 1994 -0600"
+  import 1.1.75 "Wed Dec 28 12:00:00 1994 -0600"
+  import 1.1.76 "Sun Jan 1 12:00:00 1995 -0600"
+  import 1.1.77 "Sun Jan 1 12:00:00 1995 -0600"
+  import 1.1.78 "Sun Jan 8 12:00:00 1995 -0600"
+  import 1.1.79 "Tue Jan 10 12:00:00 1995 -0600"
+  import 1.1.80 "Wed Jan 11 12:00:00 1995 -0600"
+  import 1.1.81 "Thu Jan 12 12:00:00 1995 -0600"
+  import 1.1.82 ""
+  import 1.1.83 ""
+  import 1.1.84 ""
+  import 1.1.85 ""
+  import 1.1.86 ""
+  import 1.1.87 ""
+  import 1.1.88 ""
+  import 1.1.89 ""
+  import 1.1.90 "Tue Feb 7 12:00:00 1995 -0600"
+  import 1.1.91 ""
+  import 1.1.92 ""
+  import 1.1.93 ""
+  import 1.1.94 ""
+  import 1.1.95 "Wed Mar 1 12:00:00 1995 -0600"
 
   # Now do 1.2
-  import 1.2.0
-  import 1.2.1
-  import 1.2.2
-  import 1.2.3
-  import 1.2.4
-  import 1.2.5
-  import 1.2.6
-  import 1.2.7
-  import 1.2.8
-  import 1.2.9
-  import 1.2.10
+  import 1.2.0 "Mon Mar 6 12:00:00 1995 -0600"
+  import 1.2.1 ""
+  import 1.2.2 ""
+  import 1.2.3 ""
+  import 1.2.4 "Wed Apr 5 11:00:00 1995 -0600"
+  import 1.2.5 ""
+  import 1.2.6 ""
+  import 1.2.7 ""
+  import 1.2.8 "Tue May 2 11:00:00 1995 -0600"
+  import 1.2.9 ""
+  import 1.2.10 ""
 
   # 1.2 branched to 1.3 at 1.2.10
   git branch 1.2
   git checkout 1.2
-  import 1.2.11
-  import 1.2.12
-  import 1.2.13
+  import 1.2.11 ""
+  import 1.2.12 ""
+  import 1.2.13 ""
 
   # Now do 1.3
   git checkout master
 
-  import 1.3.0
-  import 1.3.1
-  import 1.3.2
-  import 1.3.3
-  import 1.3.4
-  import 1.3.5
-  import 1.3.6
-  import 1.3.7
-  import 1.3.8
-  import 1.3.9
-  import 1.3.10
-  import 1.3.11
-  import 1.3.12
-  import 1.3.13
-  import 1.3.14
-  import 1.3.15
-  import 1.3.16
-  import 1.3.17
-  import 1.3.18
-  import 1.3.19
-  import 1.3.20
-  import 1.3.21
-  import 1.3.22
-  import 1.3.23
-  import 1.3.24
-  import 1.3.25
-  import 1.3.26
-  import 1.3.27
-  import 1.3.28
-  import 1.3.29
-  import 1.3.30
-  import 1.3.31
-  import 1.3.32
-  import 1.3.33
-  import 1.3.34
-  import 1.3.35
-  import 1.3.36
-  import 1.3.37
-  import 1.3.38
-  import 1.3.39
-  import 1.3.40
-  import 1.3.41
-  import 1.3.42
-  import 1.3.43
-  import 1.3.44
-  import 1.3.45
-  import 1.3.46
-  import 1.3.47
-  import 1.3.48
-  import 1.3.49
-  import 1.3.50
-  import 1.3.51
-  import 1.3.52
-  import 1.3.53
-  import 1.3.54
-  import 1.3.55
-  import 1.3.56
-  import 1.3.57
-  import 1.3.58
-  import 1.3.59
-  import 1.3.60
-  import 1.3.61
-  import 1.3.62
-  import 1.3.63
-  import 1.3.64
-  import 1.3.65
-  import 1.3.66
-  import 1.3.67
-  import 1.3.68
-  import 1.3.69
-  import 1.3.70
-  import 1.3.71
-  import 1.3.72
-  import 1.3.73
-  import 1.3.74
-  import 1.3.75
-  import 1.3.76
-  import 1.3.77
-  import 1.3.78
-  import 1.3.79
-  import 1.3.80
-  import 1.3.81
-  import 1.3.82
-  import 1.3.83
-  import 1.3.84
-  import 1.3.85
-  import 1.3.86
-  import 1.3.87
-  import 1.3.88
-  import 1.3.89
-  import 1.3.90
-  import 1.3.91
-  import 1.3.92
-  import 1.3.93
-  import 1.3.94
-  import 1.3.95
-  import 1.3.96
-  import 1.3.97
-  import 1.3.98
-  import 1.3.99
-  import 1.3.100
+  import 1.3.0 "Sun Jun 11 11:00:00 1995 -0600"
+  import 1.3.1 ""
+  import 1.3.2 ""
+  import 1.3.3 ""
+  import 1.3.4 ""
+  import 1.3.5 ""
+  import 1.3.6 ""
+  import 1.3.7 ""
+  import 1.3.8 ""
+  import 1.3.9 ""
+  import 1.3.10 ""
+  import 1.3.11 ""
+  import 1.3.12 ""
+  import 1.3.13 ""
+  import 1.3.14 ""
+  import 1.3.15 ""
+  import 1.3.16 ""
+  import 1.3.17 ""
+  import 1.3.18 ""
+  import 1.3.19 ""
+  import 1.3.20 ""
+  import 1.3.21 ""
+  import 1.3.22 ""
+  import 1.3.23 ""
+  import 1.3.24 ""
+  import 1.3.25 ""
+  import 1.3.26 ""
+  import 1.3.27 ""
+  import 1.3.28 ""
+  import 1.3.29 ""
+  import 1.3.30 ""
+  import 1.3.31 ""
+  import 1.3.32 ""
+  import 1.3.33 ""
+  import 1.3.34 ""
+  import 1.3.35 ""
+  import 1.3.36 ""
+  import 1.3.37 ""
+  import 1.3.38 ""
+  import 1.3.39 ""
+  import 1.3.40 ""
+  import 1.3.41 ""
+  import 1.3.42 ""
+  import 1.3.43 ""
+  import 1.3.44 ""
+  import 1.3.45 ""
+  import 1.3.46 ""
+  import 1.3.47 ""
+  import 1.3.48 ""
+  import 1.3.49 ""
+  import 1.3.50 ""
+  import 1.3.51 ""
+  import 1.3.52 "Thu Dec 28 12:00:00 1995 -0600"
+  import 1.3.53 ""
+  import 1.3.54 ""
+  import 1.3.55 ""
+  import 1.3.56 ""
+  import 1.3.57 ""
+  import 1.3.58 ""
+  import 1.3.59 ""
+  import 1.3.60 ""
+  import 1.3.61 ""
+  import 1.3.62 ""
+  import 1.3.63 ""
+  import 1.3.64 ""
+  import 1.3.65 ""
+  import 1.3.66 ""
+  import 1.3.67 ""
+  import 1.3.68 ""
+  import 1.3.69 ""
+  import 1.3.70 ""
+  import 1.3.71 ""
+  import 1.3.72 "Thu Mar 7 12:00:00 1996 -0600"
+  import 1.3.73 ""
+  import 1.3.74 ""
+  import 1.3.75 ""
+  import 1.3.76 ""
+  import 1.3.77 ""
+  import 1.3.78 ""
+  import 1.3.79 ""
+  import 1.3.80 ""
+  import 1.3.81 ""
+  import 1.3.82 ""
+  import 1.3.83 ""
+  import 1.3.84 ""
+  import 1.3.85 ""
+  import 1.3.86 ""
+  import 1.3.87 ""
+  import 1.3.88 ""
+  import 1.3.89 ""
+  import 1.3.90 ""
+  import 1.3.91 ""
+  import 1.3.92 ""
+  import 1.3.93 ""
+  import 1.3.94 ""
+  import 1.3.95 ""
+  import 1.3.96 ""
+  import 1.3.97 ""
+  import 1.3.98 "Fri May 3 11:00:00 1996 -0600"
+  import 1.3.99 ""
+  import 1.3.100 "Thu May 9 11:00:00 1996 -0600"
 
-  import pre2.0.1
-  import pre2.0.2
-  import pre2.0.3
-  import pre2.0.4
-  import pre2.0.5
-  import pre2.0.6
-  import pre2.0.7
-  import pre2.0.8
-  import pre2.0.9
-  import pre2.0.10
-  import pre2.0.11
-  import pre2.0.12
-  import pre2.0.13
-  import pre2.0.14
+  import pre2.0.1 "Sat May 11 11:00:00 1996 -0600"
+  import pre2.0.2 ""
+  import pre2.0.3 ""
+  import pre2.0.4 ""
+  import pre2.0.5 ""
+  import pre2.0.6 ""
+  import pre2.0.7 ""
+  import pre2.0.8 ""
+  import pre2.0.9 ""
+  import pre2.0.10 ""
+  import pre2.0.11 "Sun Jun 2 11:00:00 1996 -0600"
+  import pre2.0.12 ""
+  import pre2.0.13 ""
+  import pre2.0.14 ""
 }
