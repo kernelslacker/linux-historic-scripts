12aa37e4c6dc7c4fcc3efd53e221f7dc87cbdab0
Dave Jones <davej@codemonkey.org.uk>
2015-05-13 22:26:47 -0400

    remove for loops from 2.0 import

--- import-2.0.sh
+++ import-2.0.sh
@@ -3,10 +3,27 @@
 import_2_0()
 {
   import 2.0
-  for ver in $(seq 1 21)
-  do
-    import 2.0.$ver
-  done
+  import 2.0.1
+  import 2.0.2
+  import 2.0.3
+  import 2.0.4
+  import 2.0.5
+  import 2.0.6
+  import 2.0.7
+  import 2.0.8
+  import 2.0.9
+  import 2.0.10
+  import 2.0.11
+  import 2.0.12
+  import 2.0.13
+  import 2.0.14
+  import 2.0.15
+  import 2.0.16
+  import 2.0.17
+  import 2.0.18
+  import 2.0.19
+  import 2.0.20
+  import 2.0.21
 
   # 2.0 branched to 2.1 at 2.0.21
   GIT_AUTHOR_EMAIL="alan@lxorguk.ukuu.org.uk"
@@ -15,17 +32,28 @@ import_2_0()
   GIT_COMMITTER_NAME="Alan Cox"
   git branch 2.0
   git checkout 2.0
-  for ver in $(seq 22 30)
-  do
-    import 2.0.$ver
-  done
+
+  import 2.0.22
+  import 2.0.23
+  import 2.0.24
+  import 2.0.25
+  import 2.0.26
+  import 2.0.27
+  import 2.0.28
+  import 2.0.29
+  import 2.0.30
 
   import 2.0.31pre1
+  import 2.0.31pre2
+  import 2.0.31pre3
+  import 2.0.31pre4
+  import 2.0.31pre5
+  import 2.0.31pre6
+  import 2.0.31pre7
+  import 2.0.31pre8
+  import 2.0.31pre9
+  import 2.0.31pre10
 
-  for ver in $(seq 2 10)
-  do
-    import 2.0.31pre$ver
-  done
   import 2.0.31
 
   # MISSING: 2.0.32pre3 2.0.32pre4
@@ -40,32 +68,73 @@ import_2_0()
   import 2.0.33pre3 
   import 2.0.33
 
+  import 2.0.34pre2
+  import 2.0.34pre3
+  import 2.0.34pre4
+  import 2.0.34pre5
+  import 2.0.34pre6
+  import 2.0.34pre7
+  import 2.0.34pre8
+  import 2.0.34pre9
+  import 2.0.34pre10
+  import 2.0.34pre11b
+  # MISSING: pre12 is lost
   # BROKEN: pre13 and 14 don't apply
-  for ver in $(seq 2 10) 11b $(seq 15 16)
-  do
-    import 2.0.34pre$ver
-  done
+  import 2.0.34pre15
+  import 2.0.34pre16
+
   # 2.0.34pre16 == 2.0.34
   #import 2.0.34
   git tag 2.0.34
 
-  for ver in $(seq 1 9)
-  do
-    import 2.0.35pre$ver
-  done
+  import 2.0.35pre1
+  import 2.0.35pre2
+  import 2.0.35pre3
+  import 2.0.35pre4
+  import 2.0.35pre5
+  import 2.0.35pre6
+  import 2.0.35pre7
+  import 2.0.35pre8
+  import 2.0.35pre9
   import 2.0.35
 
-  for ver in $(seq 1 22)
-  do
-    import 2.0.36pre$ver
-  done
+  import 2.0.36pre1
+  import 2.0.36pre2
+  import 2.0.36pre3
+  import 2.0.36pre4
+  import 2.0.36pre5
+  import 2.0.36pre6
+  import 2.0.36pre7
+  import 2.0.36pre8
+  import 2.0.36pre9
+  import 2.0.36pre10
+  import 2.0.36pre11
+  import 2.0.36pre12
+  import 2.0.36pre13
+  import 2.0.36pre14
+  import 2.0.36pre15
+  import 2.0.36pre16
+  import 2.0.36pre17
+  import 2.0.36pre18
+  import 2.0.36pre19
+  import 2.0.36pre20
+  import 2.0.36pre21
+  import 2.0.36pre22
   # 2.0.36pre22 was released as 2.0.36 without change.
   git tag 2.0.36
 
-  for ver in $(seq 1 12)
-  do
-    import 2.0.37pre$ver
-  done
+  import 2.0.37pre1
+  import 2.0.37pre2
+  import 2.0.37pre3
+  import 2.0.37pre4
+  import 2.0.37pre5
+  import 2.0.37pre6
+  import 2.0.37pre7
+  import 2.0.37pre8
+  import 2.0.37pre9
+  import 2.0.37pre10
+  import 2.0.37pre11
+  import 2.0.37pre12
   import 2.0.37
 
   import 2.0.38pre1
@@ -76,10 +145,14 @@ import_2_0()
   GIT_AUTHOR_NAME="David Weinehall"
   GIT_COMMITTER_EMAIL="tao@acc.umu.se"
   GIT_COMMITTER_NAME="David Weinehall"
-  for ver in $(seq 1 8)
-  do
-    import 2.0.39pre$ver
-  done
+  import 2.0.39pre1
+  import 2.0.39pre2
+  import 2.0.39pre3
+  import 2.0.39pre4
+  import 2.0.39pre5
+  import 2.0.39pre6
+  import 2.0.39pre7
+  import 2.0.39pre8
   import 2.0.39
 
   import 2.0.40pre1
