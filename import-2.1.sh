f511ffca19f8837d79dc9998bac3e7b003be138a
Dave Jones <davej@codemonkey.org.uk>
2015-06-01 22:26:21 -0400

    import 2.1 dates from tarballs

--- import-2.1.sh
+++ import-2.1.sh
@@ -5,278 +5,278 @@ import_2_1()
   git checkout master
   linus
 
-  import 2.1.0
-  import 2.1.1
-  import 2.1.2
-  import 2.1.3
-  import 2.1.4
-  import 2.1.5
-  import 2.1.6
-  import 2.1.7
-  import 2.1.8
-  import 2.1.9
-  import 2.1.10
-  import 2.1.11
-  import 2.1.12
-  import 2.1.13
-  import 2.1.14
-  import 2.1.15
-  import 2.1.16
-  import 2.1.17
-  import 2.1.18
-  import 2.1.19
-  import 2.1.20
-  import 2.1.21
-  import 2.1.22
-  import 2.1.23pre1
-  import 2.1.23
-  import 2.1.24
-  import 2.1.25
-  import 2.1.26
-  import 2.1.27
-  import 2.1.28pre1
-  import 2.1.28
-  import 2.1.29
-  import 2.1.30
-  import 2.1.31
-  import 2.1.32
-  import 2.1.33
-  import 2.1.34
-  import 2.1.35
-  import 2.1.36pre1 
-  import 2.1.36
-  import 2.1.37pre1 
-  import 2.1.37pre2 
-  import 2.1.37pre3 
-  import 2.1.37pre4 
-  import 2.1.37pre5 
-  import 2.1.37pre6 
-  import 2.1.37pre7 
-  import 2.1.37
-  import 2.1.38pre1 
-  import 2.1.38
-  import 2.1.39
-  import 2.1.40
-  import 2.1.41
-  import 2.1.42pre1 
-  import 2.1.42pre2 
-  import 2.1.42
-  import 2.1.43pre1 
-  import 2.1.43
-  import 2.1.44pre2 
-  import 2.1.44pre3 
-  import 2.1.44
-  import 2.1.45pre1 
-  import 2.1.45pre2 
-  import 2.1.45pre3 
-  import 2.1.45pre4 
-  import 2.1.45pre5 
-  import 2.1.45pre6 
-  import 2.1.45pre7 
-  import 2.1.45pre8 
-  import 2.1.45pre9 
-  import 2.1.45pre10 
-  import 2.1.45
-  import 2.1.46pre1 
-  import 2.1.46
-  import 2.1.47
-  import 2.1.48pre1 
-  import 2.1.48pre2 
-  import 2.1.48pre3 
-  import 2.1.48pre4 
-  import 2.1.48
-  import 2.1.49pre1 
-  import 2.1.49
-  import 2.1.50
-  import 2.1.51pre1
-  import 2.1.51
+  import 2.1.0       "Mon Sep 30 09:53:00 1996 +0000"
+  import 2.1.1       "Thu Oct  3 08:40:00 1996 +0000"
+  import 2.1.2       "Tue Oct  8 12:15:00 1996 +0000"
+  import 2.1.3       "Thu Oct 10 10:45:00 1996 +0000"
+  import 2.1.4       "Tue Oct 15 12:52:00 1996 +0000"
+  import 2.1.5       "Fri Oct 18 07:51:00 1996 +0000"
+  import 2.1.6       "Tue Oct 29 10:38:00 1996 +0000"
+  import 2.1.7       "Fri Nov  1 09:16:00 1996 +0000"
+  import 2.1.8       "Sat Nov  9 04:27:00 1996 +0000"
+  import 2.1.9       "Tue Nov 12 08:35:00 1996 +0000"
+  import 2.1.10      "Fri Nov 15 09:09:00 1996 +0000"
+  import 2.1.11      "Mon Nov 18 17:25:00 1996 +0000"
+  import 2.1.12      "Fri Nov 22 10:57:00 1996 +0000"
+  import 2.1.13      "Sat Nov 23 05:31:00 1996 +0000"
+  import 2.1.14      "Sun Dec  1 12:38:00 1996 +0000"
+  import 2.1.15      "Thu Dec 12 10:43:00 1996 +0000"
+  import 2.1.16      "Wed Dec 18 05:57:00 1996 +0000"
+  import 2.1.17      "Sun Dec 22 04:40:00 1996 +0000"
+  import 2.1.18      "Sun Dec 29 04:39:00 1996 +0000"
+  import 2.1.19      "Tue Dec 31 08:31:00 1996 +0000"
+  import 2.1.20      "Thu Jan  2 08:46:00 1997 +0000"
+  import 2.1.21      "Tue Jan 14 06:48:00 1997 +0000"
+  import 2.1.22      "Mon Jan 20 02:36:00 1997 +0000"
+  import 2.1.23pre1  ""
+  import 2.1.23      "Sun Jan 26 07:26:00 1997 +0000"
+  import 2.1.24      "Tue Jan 28 12:22:00 1997 +0000"
+  import 2.1.25      "Sun Feb  2 10:46:00 1997 +0000"
+  import 2.1.26      "Fri Feb  7 09:49:00 1997 +0000"
+  import 2.1.27      "Wed Feb 26 17:15:00 1997 +0000"
+  import 2.1.28pre1  ""
+  import 2.1.28      "Mon Mar  3 17:36:00 1997 +0000"
+  import 2.1.29      "Mon Mar 10 15:48:00 1997 +0000"
+  import 2.1.30      "Wed Mar 26 14:59:00 1997 +0000"
+  import 2.1.31      "Wed Apr  2 23:26:00 1997 +0000"
+  import 2.1.32      "Sat Apr  5 15:47:00 1997 +0000"
+  import 2.1.33      "Thu Apr 10 14:38:00 1997 +0000"
+  import 2.1.34      "Mon Apr 14 17:23:00 1997 +0000"
+  import 2.1.35      "Tue Apr 15 19:39:00 1997 +0000"
+  import 2.1.36pre1  ""
+  import 2.1.36      "Wed Apr 23 14:18:00 1997 +0000"
+  import 2.1.37pre1  ""
+  import 2.1.37pre2  ""
+  import 2.1.37pre3  ""
+  import 2.1.37pre4  ""
+  import 2.1.37pre5  ""
+  import 2.1.37pre6  ""
+  import 2.1.37pre7  ""
+  import 2.1.37      "Wed May 14 01:15:00 1997 +0000"
+  import 2.1.38pre1  ""
+  import 2.1.38      "Thu May 15 19:02:00 1997 +0000"
+  import 2.1.39      "Sun May 18 14:09:00 1997 +0000"
+  import 2.1.40      "Wed May 21 22:02:00 1997 +0000"
+  import 2.1.41      "Tue May 27 23:49:00 1997 +0000"
+  import 2.1.42pre1  ""
+  import 2.1.42pre2  ""
+  import 2.1.42      "Thu May 29 15:26:00 1997 +0000"
+  import 2.1.43pre1  ""
+  import 2.1.43      "Mon Jun 16 18:33:00 1997 +0000"
+  import 2.1.44pre2  ""
+  import 2.1.44pre3  ""
+  import 2.1.44      "Mon Jul  7 19:38:00 1997 +0000"
+  import 2.1.45pre1  ""
+  import 2.1.45pre2  ""
+  import 2.1.45pre3  ""
+  import 2.1.45pre4  ""
+  import 2.1.45pre5  ""
+  import 2.1.45pre6  ""
+  import 2.1.45pre7  ""
+  import 2.1.45pre8  ""
+  import 2.1.45pre9  ""
+  import 2.1.45pre10 ""
+  import 2.1.45      "Wed Jul 16 23:37:00 1997 +0000"
+  import 2.1.46pre1  ""
+  import 2.1.46      "Sat Jul 19 15:53:00 1997 +0000"
+  import 2.1.47      "Thu Jul 24 15:30:00 1997 +0000"
+  import 2.1.48pre1  ""
+  import 2.1.48pre2  ""
+  import 2.1.48pre3  ""
+  import 2.1.48pre4  ""
+  import 2.1.48      "Mon Aug  4 18:40:00 1997 +0000"
+  import 2.1.49pre1  ""
+  import 2.1.49      "Mon Aug 11 17:37:00 1997 +0000"
+  import 2.1.50      "Thu Aug 14 17:54:00 1997 +0000"
+  import 2.1.51pre1  ""
+  import 2.1.51      "Mon Aug 18 20:16:00 1997 +0000"
 #WTF?
-#  import 2.1.52pre1 
-#  import 2.1.52pre2
-  import 2.1.52
-  import 2.1.53
-  import 2.1.54
-  import 2.1.55pre1
-  import 2.1.55
-  import 2.1.56pre1 
-  import 2.1.56
-  import 2.1.57
-  import 2.1.58
-  import 2.1.59
-  import 2.1.60
-  import 2.1.61
-  import 2.1.62
-  import 2.1.63
-  import 2.1.64
-  import 2.1.65
-  import 2.1.66
-  import 2.1.67
-  import 2.1.68pre1 
-  import 2.1.68
-  import 2.1.69
-  import 2.1.70
-  import 2.1.71
-  import 2.1.72
-  import 2.1.73
-  import 2.1.74
-  import 2.1.75
-  import 2.1.76
-  import 2.1.77
-  import 2.1.78pre1 
-  import 2.1.78pre2 
-  import 2.1.78pre3 
-  import 2.1.78
-  import 2.1.79pre1 
-  import 2.1.79
-  import 2.1.80pre1 
-  import 2.1.80pre2 
-  import 2.1.80pre3 
-  import 2.1.80pre4 
-  import 2.1.80
-  import 2.1.81pre1 
-  import 2.1.81
-  import 2.1.82
-  import 2.1.83
-  import 2.1.84
-  import 2.1.85
-  import 2.1.86
-  import 2.1.87pre1 
-  import 2.1.87
-  import 2.1.88
-  import 2.1.89pre1 
-  import 2.1.89pre2 
-  import 2.1.89pre3 
-  import 2.1.89pre4 
-  import 2.1.89pre5 
-  import 2.1.89
-  import 2.1.90pre1 
-  import 2.1.90pre2 
-  import 2.1.90pre3 
-  import 2.1.90
-  import 2.1.91pre1 
-  import 2.1.91pre2 
-  import 2.1.91
-  import 2.1.92pre1 
-  import 2.1.92pre2 
-  import 2.1.92
-  import 2.1.93
-  import 2.1.94
-  import 2.1.95pre1 
-  import 2.1.95
-  import 2.1.96pre1 
-  import 2.1.96
-  import 2.1.97
-  import 2.1.98
-  import 2.1.99pre1
-  import 2.1.99pre2 
-  import 2.1.99pre3 
-  import 2.1.99
-  import 2.1.100pre1 
-  import 2.1.100pre2 
-  import 2.1.100pre3 
-  import 2.1.100
-  import 2.1.101
-  import 2.1.102pre1 
-  import 2.1.102pre2 
-  import 2.1.102
-  import 2.1.103
-  import 2.1.104pre1 
-  import 2.1.104
-  import 2.1.105
-  import 2.1.106pre1 
-  import 2.1.106
-  import 2.1.107pre1 
-  import 2.1.107pre2 
-  import 2.1.107
-  import 2.1.108pre1 
-  import 2.1.108
-  import 2.1.109pre1 
-  import 2.1.109pre2 
-  import 2.1.109
-  import 2.1.110pre1 
-  import 2.1.110pre2 
-  import 2.1.110pre3 
-  import 2.1.110
-  import 2.1.111pre1 
-  import 2.1.111
-  import 2.1.112pre1 
-  import 2.1.112pre2 
-  import 2.1.112
-  import 2.1.113
-  import 2.1.114
-  import 2.1.115pre1 
-  import 2.1.115pre2 
-  import 2.1.115pre3 
-  import 2.1.115pre4 
-  import 2.1.115
-  import 2.1.116pre1 
-  import 2.1.116pre2 
-  import 2.1.116
-  import 2.1.117pre1 
-  import 2.1.117
-  import 2.1.118
-  import 2.1.119pre1 
-  import 2.1.119
-  import 2.1.120pre1 
-  import 2.1.120pre2 
-  import 2.1.120pre3 
-  import 2.1.120
-  import 2.1.121pre1 
-  import 2.1.121
-  import 2.1.122pre1 
-  import 2.1.122pre2 
-  import 2.1.122pre3 
-  import 2.1.122
-  import 2.1.123pre1 
-  import 2.1.123pre2 
-  import 2.1.123pre3 
-  import 2.1.123
-  import 2.1.124pre1 
-  import 2.1.124pre2 
-  import 2.1.124
-  import 2.1.125pre1 
-  import 2.1.125pre2 
-  import 2.1.125
-  import 2.1.126pre1 
-  import 2.1.126pre2 
-  import 2.1.126
-  import 2.1.127pre1 
-  import 2.1.127pre2 
-  import 2.1.127pre3 
-  import 2.1.127pre6 
-  import 2.1.127pre7 
-  import 2.1.127
-  import 2.1.128pre1 
-  import 2.1.128
-  import 2.1.129pre1 
-  import 2.1.129pre2 
-  import 2.1.129pre3 
-  import 2.1.129pre4 
-  import 2.1.129pre5 
-  import 2.1.129pre6 
-  import 2.1.129
-  import 2.1.130pre2 
-  import 2.1.130pre3 
-  import 2.1.130
-  import 2.1.131pre2 
-  import 2.1.131pre3 
-  import 2.1.131
-  import 2.1.132pre1 
-  import 2.1.132pre2 
-  import 2.1.132pre3 
-  import 2.1.132pre4 
-  import 2.1.132
-  import 2.1.133pre1 
-  import 2.1.133pre3 
-  import 2.1.133pre4 
-  import 2.1.133pre5 
-  import 2.2.0pre1 
-  import 2.2.0pre2 
-  import 2.2.0pre3 
-  import 2.2.0pre4 
-  import 2.2.0pre5 
-  import 2.2.0pre6 
-  import 2.2.0pre7 
-  import 2.2.0pre8 
-  import 2.2.0pre9
+#  import 2.1.52pre1  ""
+#  import 2.1.52pre2 ""
+  import 2.1.52      "Wed Sep  3 14:44:00 1997 +0000"
+  import 2.1.53      "Thu Sep  4 18:42:00 1997 +0000"
+  import 2.1.54      "Sat Sep  6 14:15:00 1997 +0000"
+  import 2.1.55pre1  ""
+  import 2.1.55      "Tue Sep  9 17:35:00 1997 +0000"
+  import 2.1.56pre1  ""
+  import 2.1.56      "Sat Sep 20 01:09:00 1997 +0000"
+  import 2.1.57      "Wed Sep 24 18:49:00 1997 +0000"
+  import 2.1.58      "Wed Oct 15 13:32:00 1997 +0000"
+  import 2.1.59      "Fri Oct 17 19:18:00 1997 +0000"
+  import 2.1.60      "Sat Oct 25 05:06:00 1997 +0000"
+  import 2.1.61      "Thu Oct 30 18:49:00 1997 +0000"
+  import 2.1.62      "Mon Nov  3 15:29:00 1997 +0000"
+  import 2.1.63      "Wed Nov 12 14:11:00 1997 +0000"
+  import 2.1.64      "Fri Nov 14 21:57:00 1997 +0000"
+  import 2.1.65      "Mon Nov 17 21:48:00 1997 +0000"
+  import 2.1.66      "Tue Nov 25 19:08:00 1997 +0000"
+  import 2.1.67      "Sat Nov 29 14:12:00 1997 +0000"
+  import 2.1.68pre1  ""
+  import 2.1.68      "Sun Nov 30 18:00:00 1997 +0000"
+  import 2.1.69      "Mon Dec  1 16:37:00 1997 +0000"
+  import 2.1.70      "Tue Dec  2 19:04:00 1997 +0000"
+  import 2.1.71      "Thu Dec  4 17:40:00 1997 +0000"
+  import 2.1.72      "Tue Dec  9 15:49:00 1997 +0000"
+  import 2.1.73      "Thu Dec 18 19:41:00 1997 +0000"
+  import 2.1.74      "Fri Dec 19 19:42:00 1997 +0000"
+  import 2.1.75      "Sun Dec 21 21:03:00 1997 +0000"
+  import 2.1.76      "Tue Dec 23 19:06:00 1997 +0000"
+  import 2.1.77      "Fri Jan  2 17:23:00 1998 +0000"
+  import 2.1.78pre1  ""
+  import 2.1.78pre2  ""
+  import 2.1.78pre3  ""
+  import 2.1.78      "Mon Jan  5 20:55:00 1998 +0000"
+  import 2.1.79pre1  ""
+  import 2.1.79      "Mon Jan 12 23:14:00 1998 +0000"
+  import 2.1.80pre1  ""
+  import 2.1.80pre2  ""
+  import 2.1.80pre3  ""
+  import 2.1.80pre4  ""
+  import 2.1.80      "Tue Jan 20 20:00:00 1998 +0000"
+  import 2.1.81pre1  ""
+  import 2.1.81      "Fri Jan 23 20:38:00 1998 +0000"
+  import 2.1.82      "Mon Jan 26 17:53:00 1998 +0000"
+  import 2.1.83      "Fri Jan 30 18:09:00 1998 +0000"
+  import 2.1.84      "Fri Jan 30 20:01:00 1998 +0000"
+  import 2.1.85      "Tue Feb  3 21:23:00 1998 +0000"
+  import 2.1.86      "Tue Feb 10 19:20:00 1998 +0000"
+  import 2.1.87pre1  ""
+  import 2.1.87      "Mon Feb 16 20:08:00 1998 +0000"
+  import 2.1.88      "Fri Feb 20 22:19:00 1998 +0000"
+  import 2.1.89pre1  ""
+  import 2.1.89pre2  ""
+  import 2.1.89pre3  ""
+  import 2.1.89pre4  ""
+  import 2.1.89pre5  ""
+  import 2.1.89      "Sat Mar 7 02:24:00 1998 +0000"
+  import 2.1.90pre1  ""
+  import 2.1.90pre2  ""
+  import 2.1.90pre3  ""
+  import 2.1.90      "Wed Mar 18 01:12:00 1998 +0000"
+  import 2.1.91pre1  ""
+  import 2.1.91pre2  ""
+  import 2.1.91      "Thu Mar 26 18:45:00 1998 +0000"
+  import 2.1.92pre1  ""
+  import 2.1.92pre2  ""
+  import 2.1.92      "Wed Apr  1 20:39:00 1998 +0000"
+  import 2.1.93      "Mon Apr  6 20:28:00 1998 +0000"
+  import 2.1.94      "Wed Apr  8 22:09:00 1998 +0000"
+  import 2.1.95pre1  ""
+  import 2.1.95      "Fri Apr 10 14:16:00 1998 +0000"
+  import 2.1.96pre1  ""
+  import 2.1.96      "Tue Apr 14 16:12:00 1998 +0000"
+  import 2.1.97      "Sat Apr 18 01:08:00 1998 +0000"
+  import 2.1.98      "Fri Apr 24 01:42:00 1998 +0000"
+  import 2.1.99pre1  ""
+  import 2.1.99pre2  ""
+  import 2.1.99pre3  ""
+  import 2.1.99      "Thu Apr 30 19:19:00 1998 +0000"
+  import 2.1.100pre1 ""
+  import 2.1.100pre2 ""
+  import 2.1.100pre3 ""
+  import 2.1.100     "Thu May  7 22:04:00 1998 +0000"
+  import 2.1.101     "Fri May  8 21:43:00 1998 +0000"
+  import 2.1.102pre1 ""
+  import 2.1.102pre2 ""
+  import 2.1.102     "Thu May 14 19:13:00 1998 +0000"
+  import 2.1.103     "Wed May 20 19:33:00 1998 +0000"
+  import 2.1.104pre1 ""
+  import 2.1.104     "Fri Jun  5 03:05:00 1998 +0000"
+  import 2.1.105     "Sun Jun  7 14:27:00 1998 +0000"
+  import 2.1.106pre1 ""
+  import 2.1.106     "Sat Jun 13 16:59:00 1998 +0000"
+  import 2.1.107pre1 ""
+  import 2.1.107pre2 ""
+  import 2.1.107     "Thu Jun 25 00:58:00 1998 +0000"
+  import 2.1.108pre1 ""
+  import 2.1.108     "Wed Jul  1 11:29:00 1998 +0000"
+  import 2.1.109pre1 ""
+  import 2.1.109pre2 ""
+  import 2.1.109     "Thu Jul 16 19:31:00 1998 +0000"
+  import 2.1.110pre1 ""
+  import 2.1.110pre2 ""
+  import 2.1.110pre3 ""
+  import 2.1.110     "Tue Jul 21 02:48:00 1998 +0000"
+  import 2.1.111pre1 ""
+  import 2.1.111     "Sat Jul 25 02:17:00 1998 +0000"
+  import 2.1.112pre1 ""
+  import 2.1.112pre2 ""
+  import 2.1.112     "Tue Jul 28 16:55:00 1998 +0000"
+  import 2.1.113     "Sat Aug  1 02:44:00 1998 +0000"
+  import 2.1.114     "Mon Aug  3 16:08:00 1998 +0000"
+  import 2.1.115pre1 ""
+  import 2.1.115pre2 ""
+  import 2.1.115pre3 ""
+  import 2.1.115pre4 ""
+  import 2.1.115     "Thu Aug  6 16:23:00 1998 +0000"
+  import 2.1.116pre1 ""
+  import 2.1.116pre2 ""
+  import 2.1.116     "Tue Aug 18 20:52:00 1998 +0000"
+  import 2.1.117pre1 ""
+  import 2.1.117     "Wed Aug 19 22:22:00 1998 +0000"
+  import 2.1.118     "Tue Aug 25 20:42:00 1998 +0000"
+  import 2.1.119pre1 ""
+  import 2.1.119     "Thu Aug 27 16:28:00 1998 +0000"
+  import 2.1.120pre1 ""
+  import 2.1.120pre2 ""
+  import 2.1.120pre3 ""
+  import 2.1.120     "Sat Sep  5 02:46:00 1998 +0000"
+  import 2.1.121pre1 ""
+  import 2.1.121     "Wed Sep  9 17:06:00 1998 +0000"
+  import 2.1.122pre1 ""
+  import 2.1.122pre2 ""
+  import 2.1.122pre3 ""
+  import 2.1.122     "Wed Sep 16 18:12:00 1998 +0000"
+  import 2.1.123pre1 ""
+  import 2.1.123pre2 ""
+  import 2.1.123pre3 ""
+  import 2.1.123     "Sun Sep 27 23:29:00 1998 +0000"
+  import 2.1.124pre1 ""
+  import 2.1.124pre2 ""
+  import 2.1.124     "Sun Oct  4 15:23:00 1998 +0000"
+  import 2.1.125pre1 ""
+  import 2.1.125pre2 ""
+  import 2.1.125     "Fri Oct  9 00:42:00 1998 +0000"
+  import 2.1.126pre1 ""
+  import 2.1.126pre2 ""
+  import 2.1.126     "Fri Oct 23 21:08:00 1998 +0000"
+  import 2.1.127pre1 ""
+  import 2.1.127pre2 ""
+  import 2.1.127pre3 ""
+  import 2.1.127pre6 ""
+  import 2.1.127pre7 ""
+  import 2.1.127     "Sat Nov  7 14:31:00 1998 +0000"
+  import 2.1.128pre1 ""
+  import 2.1.128     "Thu Nov 12 15:02:00 1998 +0000"
+  import 2.1.129pre1 ""
+  import 2.1.129pre2 ""
+  import 2.1.129pre3 ""
+  import 2.1.129pre4 ""
+  import 2.1.129pre5 ""
+  import 2.1.129pre6 ""
+  import 2.1.129     "Thu Nov 19 02:58:00 1998 +0000"
+  import 2.1.130pre2 ""
+  import 2.1.130pre3 ""
+  import 2.1.130     "Thu Nov 26 16:14:00 1998 +0000"
+  import 2.1.131pre2 ""
+  import 2.1.131pre3 ""
+  import 2.1.131     "Wed Dec  2 20:15:00 1998 +0000"
+  import 2.1.132pre1 ""
+  import 2.1.132pre2 ""
+  import 2.1.132pre3 ""
+  import 2.1.132pre4 ""
+  import 2.1.132     "Tue Dec 22 16:04:00 1998 +0000"
+  import 2.1.133pre1 ""
+  import 2.1.133pre3 ""
+  import 2.1.133pre4 ""
+  import 2.1.133pre5 ""
+  import 2.2.0pre1   ""
+  import 2.2.0pre2   ""
+  import 2.2.0pre3   ""
+  import 2.2.0pre4   ""
+  import 2.2.0pre5   ""
+  import 2.2.0pre6   ""
+  import 2.2.0pre7   ""
+  import 2.2.0pre8   ""
+  import 2.2.0pre9   ""
 }
